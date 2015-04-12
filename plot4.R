607c2c26ff5277b21f27ea39deb34830b2d9a816
Kyle Cabigon <kyle.cabigon@gmail.com>
2015-04-12 13:44:31 -0700

    create 4 plots in plot4.R

--- plot4.R
+++ plot4.R
@@ -13,3 +13,34 @@ feb2 <- dataset[dataset$Date == date2, ]
 real <- rbind(feb1,feb2)
 real$DateTime <- as.POSIXct(paste(real$Date, real$Time), format = "%Y-%m-%d %H:%M:%S")
 
+# create 4 plots and save to PNG file
+png('plot4.png')
+par(mfrow = c(2,2))
+plot(real$DateTime, 
+     real$Global_active_power, 
+     type ="l", 
+     xlab = "", 
+     ylab = "Global Active Power (kilowatts)")
+plot(real$DateTime, 
+     real$Voltage, 
+     type ="l", 
+     xlab = "datetime", 
+     ylab = "Voltage")
+plot(real$DateTime, 
+     real$Sub_metering_1, 
+     type = "s", 
+     col = "black", 
+     xlab = "", 
+     ylab = "Energy sub metering")
+points(real$DateTime, real$Sub_metering_2, type = "s", col = "red")
+points(real$DateTime, real$Sub_metering_3, type = "s", col = "blue")
+legend("topright", 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = c(1,1,1), 
+       col = c("black", "red", "blue"))
+plot(real$DateTime, 
+     real$Global_reactive_power, 
+     type ="l", 
+     xlab = "datetime", 
+     ylab = "Global_reactive_power")
+dev.off()
